1874c713bdef4be82aa4556890648db4cfde6ce0
meszaros-lajos-gyorgy <m_lajos@hotmail.com>
2015-07-10 10:31:31 +0200

    Almost finished documenting core/Object

--- system/packages/core/Object.php
+++ system/packages/core/Object.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Base class, almost all EOF classes derivate from this.
+ * 
+ * @package core
+ */
 class Object extends stdClass{
 	const
 		UNDEFINED			= null,
@@ -14,6 +19,11 @@ class Object extends stdClass{
 		$this->getUUID();
 	}
 	
+	/**
+	 * Gives back the unique id of the current object
+	 * 
+	 * @returns {string} - unique ID
+	 */
 	public function getUUID(){
 		if($this->uuid === false){
 			$this->uuid = uniqid();
@@ -21,10 +31,18 @@ class Object extends stdClass{
 		return $this->uuid;
 	}
 	
+	/**
+	 * Clones this object
+	 * 
+	 * @returns {Object} - a copy of the current object
+	 */
 	public function copy(){
 		return clone $this;
 	}
 	
+	/**
+	 * 
+	 */
 	public static function toObject($a, $primitiveType = self::UNDEFINED, Array $params = Array()){
 		$thisClassName = get_called_class();
 		if(!self::isSameObject($a, true)){
@@ -43,6 +61,13 @@ class Object extends stdClass{
 		return $a;
 	}
 	
+	/**
+	 * Compares current object with another object
+	 * 
+	 * @param {*} $a - The object, that we want to compare the current object with
+	 * @param {boolean} $checkSubClass - Default false. If set to true, it will mark derivated classes the same too, not just exactly matching ones.
+	 * @returns {boolean}
+	 */
 	public static function isSameObject($a, $checkSubClass = false){
 		$thisClassName = get_called_class();
 		return (
